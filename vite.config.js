8353c0855873513f3d283806698781e3a90f3071
3329571576 <huy3329571576@163.com>
2025-10-23 09:50:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [vue()],
-    base: '/treasure-game-vue/', // 替换为你的仓库名
-    server: {
-        port: 3000,
-        host: true
-    },
-    build: {
-        outDir: 'dist'
-    }
+  plugins: [vue()],
+  base: './',  // 使用相对路径
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
