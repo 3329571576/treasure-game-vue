77cd61f49038bd4afded28b7e46783828324688b
3329571576 <huy3329571576@163.com>
2025-10-23 09:49:41 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,30 @@ import Leaderboard from '../views/Leaderboard.vue'
 import UserProfile from '../views/UserProfile.vue'
 
 const routes = [
-    {
-        path: '/treasure-game-vue/', // 添加仓库名前缀
-        name: 'Game',
-        component: GameView
-    },
-    {
-        path: '/treasure-game-vue/leaderboard',
-        name: 'Leaderboard',
-        component: Leaderboard
-    },
-    {
-        path: '/treasure-game-vue/profile',
-        name: 'Profile',
-        component: UserProfile
-    },
-    {
-        path: '/treasure-game-vue/:pathMatch(.*)*',
-        redirect: '/treasure-game-vue/'
-    }
+  {
+    path: '/',
+    name: 'Game',
+    component: GameView
+  },
+  {
+    path: '/leaderboard',
+    name: 'Leaderboard',
+    component: Leaderboard
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: UserProfile
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/'
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory('/treasure-game-vue/'), // 添加仓库名
-    routes
+  history: createWebHistory(),
+  routes
 })
 
 export default router
